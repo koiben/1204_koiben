0273d5244637359acc3520a72b7bf8733fdd9858
ogu1211 <ogu1211@gmail.com>
2016-12-04 15:36:34 +0900

    1536

---
+++ 1204_koiben/Type2.swift
@@ -0,0 +1,15 @@
+//
+//  Type2.swift
+//  1204_koiben
+//
+//  Created by TEST on 2016/12/04.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+import UIKit
+
+class Type2: UIViewController {
+    
+    @IBOutlet weak var Type2Tap: UIButton!
+        
+
+}
