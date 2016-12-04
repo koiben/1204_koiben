db50791bea180458816e3b38e290fcf3ecdd54c9
ogu1211 <ogu1211@gmail.com>
2016-12-04 13:58:47 +0900

    1358

--- 1204_koiben/ViewController.swift
+++ 1204_koiben/ViewController.swift
@@ -11,21 +11,27 @@ import UIKit
 class ViewController: UIViewController {
 
   
-//Type選択画面
+//Type選択(UserAct)
     //押したキャラに応じて数字をメイン画面に渡す
 
 //Main画面遷移直後
     //受け取った数字を元に背景画像を表示する
+    var img01 :UIImage = UIImage(named:"image1.jpg")!
+    var img02 :UIImage = UIImage(named:"image2.jpg")!
+    var img03 :UIImage = UIImage(named:"image3.jpg")!
+    var imgArray:[UIImage] = []
     
     //(受け取った数字を元に)CSVからコメントを表示する
     
-//恋勉をするタップ
+//恋勉をするタップ(UserAct)
+    //Main2に遷移
     //今回の学習時間を計測する
-        //会話をするをタップ
+        //会話をするをタップ(UserAct)
             //(ランダムに)CSVから会話を表示する
             //タップすると元の画面に戻る
     
-        //恋勉をやめるをタップ
+        //恋勉をやめるをタップ(UserAct)
+            //Main1に遷移
             //時間の計測を停止し、累積の学習時間に追加
             //
     
