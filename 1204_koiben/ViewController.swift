bdde02a85f8de433d742ae45d910c86e2cc752a4
ogu1211 <ogu1211@gmail.com>
2016-12-04 13:24:56 +0900

    1324

--- 1204_koiben/ViewController.swift
+++ 1204_koiben/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+//Type選択画面
+    //押したキャラに応じて数字をメイン画面に渡す
+
+//Main画面遷移直後
+    //受け取った数字を元に背景画像を表示する
+    
+    //(受け取った数字を元に)CSVからコメントを表示する
+    
+//恋勉をするタップ
+    //今回の学習時間を計測する
+        //会話をするをタップ
+            //(ランダムに)CSVから会話を表示する
+            //タップすると元の画面に戻る
+    
+        //恋勉をやめるをタップ
+            //時間の計測を停止し、累積の学習時間に追加
+            //
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
